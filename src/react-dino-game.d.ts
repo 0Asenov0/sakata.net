92de14cccd772c5a68b3e8b9b16334466fcbb545
Stoil Asenov <s.asenov352@gmail.com>
2025-10-20 12:11:01 +0300

    Clear main branch

--- src/react-dino-game.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'react-dino-game' {
-  import { ComponentType } from 'react';
-  interface DinoGameProps {
-    width?: number;
-    height?: number;
-  }
-  const DinoGame: ComponentType<DinoGameProps>;
-  export default DinoGame;
-}
